af4eff537a5cdfd53b59d5ee726ef3287fc5191f
Patrick <patrickocal@gmail.com>
2021-12-06 20:25:25 +1000

    amendments to postprocessing.py

--- Lecture_6/code/growth_model_GPR/HS071_iter.py
+++ Lecture_6/code/growth_model_GPR/HS071_iter.py
@@ -1,34 +1,37 @@
+"""
+This class is used to pass the optimization problem to cyipopt.
+"""
 
-
-from ipopt_wrapper_A import EV_F, EV_GRAD_F, EV_G, EV_JAC_G
-import numpy as np
-
+from parameters import *
+from ipopt_wrapper_A import EV_F_ITER, EV_GRAD_F_ITER, EV_G_ITER, EV_JAC_G_ITER
 
 class HS071(): 
     """
     Class for the optimization problem to be passed to cyipopt 
     Further optimisations may be possible here by including a hessian (optional param) 
+    Uses the existing instance of the Gaussian Process (GP OLD) 
     """
 
-    def __init__(self, X, n_agents, k_init, NELE_JAC, NELE_HESS): 
+    def __init__(self, X, n_agents, k_init, NELE_JAC, NELE_HESS, gp_old): 
         self.x = X 
         self.n_agents = n_agents 
         self.k_init = k_init 
         self.NELE_JAC = NELE_JAC 
-        self.NELE_HESS = NELE_HESS
+        self.NELE_HESS = NELE_HESS 
+        self.gp_old = gp_old 
 
     # Create ev_f, eval_f, eval_grad_f, eval_g, eval_jac_g for given k_init and n_agent 
     def eval_f(self, x): 
-        return EV_F(x, self.k_init, self.n_agents) 
+        return EV_F_ITER(x, self.k_init, self.n_agents, self.gp_old) 
 
     def eval_grad_f(self, x): 
-        return EV_GRAD_F(x, self.k_init, self.n_agents) 
+        return EV_GRAD_F_ITER(x, self.k_init, self.n_agents, self.gp_old) 
 
     def eval_g(self, x): 
-        return EV_G(x, self.k_init, self.n_agents) 
+        return EV_G_ITER(x, self.k_init, self.n_agents) 
 
     def eval_jac_g(self, x, flag): 
-        return EV_JAC_G(x, flag, self.k_init, self.n_agents) 
+        return EV_JAC_G_ITER(x, flag, self.k_init, self.n_agents) 
 
     def objective(self, x): 
         # Returns the scalar value of the objective given x. 
@@ -53,4 +56,4 @@ class HS071():
 
         msg = "Objective value at iteration #{:d} is - {:g}"
 
-        print(msg.format(iter_count, obj_value))
+        print(msg.format(iter_count, obj_value))    
